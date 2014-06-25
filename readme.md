259d64b255e2600950b0d6f6b0c8ffd6db9ecaed
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-06-25 10:44:59 -0400

    Automated commit. [ci skip]

--- readme.md
+++ readme.md
@@ -38,3 +38,62 @@ console.log( project.features );
 ```
 rm -rf *.* examples lib node_modules static test vendor && scaffold-library create
 ```
+
+### Mocha Example
+```js
+module.exports = {  
+  'Project': { 
+    
+    'has project-yml file.': require( 'grunt-scaffold-module' ).testProjectValidity({
+      debug: false
+    }),
+    
+    'has requied properties.': require( 'grunt-scaffold-module' ).testStructure({
+      debug: false      
+    })
+    
+  },
+
+  'API': {
+
+    'has expected methods': require( 'grunt-scaffold-module' ).testMethods({
+      debug: false
+    }),
+
+    'has valid public classess.': require( 'grunt-scaffold-module' ).testClasses({
+      debug: false
+    })
+
+  },
+
+  'Coverage': {    
+    
+    'PHP Unit Tests': require( 'grunt-scaffold-module' ).phpUnit({
+      dir: 'lib/*.php',
+      severity: '',
+      standard: 'Zend',
+      warningSeverity: '',
+      ignore: 'php'
+    }),
+    
+    'PHP Mass Detector': require( 'grunt-scaffold-module' ).phpmd({
+      dir: 'lib/*.php',
+      rulesets: 'codesize'
+    }),
+    
+    'JS Hint': require( 'grunt-scaffold-module' ).jsHint({
+      
+    }),
+
+    'nodeUnit': require( 'grunt-scaffold-module' ).nodeUnit({
+      debug: false
+    }),
+
+    'custom test': function customTest() {  
+      //console.log({'asdf': "asdf"});      
+      //console.log( this._runnable );      
+    }
+    
+  }
+}
+```
--- test/grunt-scaffold-wp-plugin.js
+++ test/grunt-scaffold-wp-plugin.js
@@ -1,56 +1,7 @@
 module.exports = {  
-  'Project': { 
-    
-    'has project-yml file.': require( 'grunt-scaffold-module' ).testProjectValidity({
-      debug: false
-    }),
-    
-    'has requied properties.': require( 'grunt-scaffold-module' ).testStructure({
-      debug: false      
-    })
-    
-  },
-
-  'API': {
-
-    'has expected methods': require( 'grunt-scaffold-module' ).testMethods({
-      debug: false
-    }),
-
-    'has valid public classess.': require( 'grunt-scaffold-module' ).testClasses({
-      debug: false
-    })
-
-  },
-
-  'Coverage': {    
-    
-    'PHP Unit Tests': require( 'grunt-scaffold-module' ).phpUnit({
-      dir: 'lib/*.php',
-      severity: '',
-      standard: 'Zend',
-      warningSeverity: '',
-      ignore: 'php'
-    }),
-    
-    'PHP Mass Detector': require( 'grunt-scaffold-module' ).phpmd({
-      dir: 'lib/*.php',
-      rulesets: 'codesize'
-    }),
-    
-    'JS Hint': require( 'grunt-scaffold-module' ).jsHint({
-      
-    }),
-
-    'nodeUnit': require( 'grunt-scaffold-module' ).nodeUnit({
-      debug: false
-    }),
-
-    'custom test': function customTest() {  
-      //console.log({'asdf': "asdf"});      
-      //console.log( this._runnable );      
-    }
-    
-  }
+  'grunt-scaffold-library': { 
+    'has valid project structure.': require( 'grunt-scaffold-module' ).testStructure(),
+    'has valid public methods.': require( 'grunt-scaffold-module' ).testMethods(),
+    'has valid public classess.': require( 'grunt-scaffold-module' ).testClasses()       
+  } 
 }
-
